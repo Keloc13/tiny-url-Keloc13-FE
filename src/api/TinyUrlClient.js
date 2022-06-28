659e6ed48a75e68200101514a4759c5988daa501
Kevin Vu Nguyen <kevvu101@Kevins-MacBook-Pro.local>
2022-06-28 11:17:05 -0500

    fixing environment variable

--- src/api/TinyUrlClient.js
+++ src/api/TinyUrlClient.js
@@ -1,7 +1,7 @@
 class TinyUrlClient {
     constructor() {}
     async triggerGetUrl(urlId) {
-        let response = await fetch(`${process.env.GET_TINY_URL}id=${urlId}`, {
+        let response = await fetch(`${process.env.REACT_APP_GET_TINY_URL}id=${urlId}`, {
             method: 'GET',
             headers: {
             "Access-Control-Allow-Origin": "*",
@@ -24,7 +24,7 @@ class TinyUrlClient {
 
      async triggerPutUrl(url) {
         console.log(`process.env.POST_TINY_URL: ${JSON.stringify(process.env)}`)
-        return await fetch(process.env.POST_TINY_URL, {
+        return await fetch(process.env.REACT_APP_POST_TINY_URL, {
             method: 'POST',
             body: JSON.stringify({
             "originalUrl": url
